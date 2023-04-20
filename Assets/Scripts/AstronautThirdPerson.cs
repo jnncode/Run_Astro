80d192acff31ebcc8e77dad17ddc406d7472bb8a
J Nguyen <n.nguyenj@yahoo.com>
2023-04-19 23:16:45 -0500

    Camera Scroll
    Update to the Player view

--- Assets/Scripts/AstronautThirdPerson.cs
+++ Assets/Scripts/AstronautThirdPerson.cs
@@ -13,6 +13,9 @@ namespace AstronautThirdPerson
     public Transform lookAt;
     public Transform camTransform;
     public float distance = 5.0f;
+    public float zoomSpeed = 1.0f;
+    public float zoomMin = 1.0f;
+    public float zoomMax = 15.0f;
 
     private float currentX = 0.0f;
     private float currentY = 45.0f;
@@ -28,6 +31,11 @@ namespace AstronautThirdPerson
         currentY += Input.GetAxis("Mouse Y");
 
         currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+        
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        distance -= scroll * zoomSpeed;
+        distance = Mathf.Clamp(distance, zoomMin, zoomMax);
     }
 
     private void LateUpdate()
